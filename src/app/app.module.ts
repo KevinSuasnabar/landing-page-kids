ab819bc811854f8d95b095a25e7a99d2ad345467
kevinSuasnabar <xkevin_157@hotmail.com>
2020-02-03 23:19:27 -0500

    template sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { QualifiersComponent } from './components/qualifiers/qualifiers.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProgramsComponent } from './components/programs/programs.component';
+import { TestimoniosComponent } from './components/testimonios/testimonios.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+import { VioletComponent } from './components/violet/violet.component';
+import { QuestionsComponent } from './components/questions/questions.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { PricingComponent } from './components/pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AdminComponent,
+    SliderComponent,
+    QualifiersComponent,
+    OffersComponent,
+    AboutComponent,
+    ProgramsComponent,
+    TestimoniosComponent,
+    TeachersComponent,
+    VioletComponent,
+    QuestionsComponent,
+    BlogsComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/qualifiers/qualifiers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qualifiers',
+  templateUrl: './qualifiers.component.html',
+  styleUrls: ['./qualifiers.component.css']
+})
+export class QualifiersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
